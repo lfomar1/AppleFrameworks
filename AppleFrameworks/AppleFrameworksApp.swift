b3aa0dd269ee884601c4932de6378bc5c11fa487
Lucas <107668539+lfomar1@users.noreply.github.com>
2024-01-31 17:47:56 -0300

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Lucas Omar on 2024-01-31.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
